d54bd0f17975114ea31beedb68f8653758f5fcf5
Angel Geovanny Hernández Alegría <99096461+GeovannyHDZ@users.noreply.github.com>
2022-03-21 20:20:45 -0600

    Update app.js

---
+++ JS Practices/app.js
@@ -0,0 +1,25 @@
+const fetchPokemon = () => {
+    const pokeNameInput = document.getElementById("pokeName");
+    let pokeName = pokeNameInput.value;
+    pokeName = pokeName.toLowerCase();
+    const url = `https://pokeapi.co/api/v2/pokemon/${pokeName}`;
+    fetch(url).then((res) => {
+        if (res.status != "200") {
+            console.log(res);
+            pokeImage("./src/Error-404.png")
+        }
+        else {
+            return res.json();
+        }
+    }).then((data) => {
+        if (data) {
+            let pokeImg = data.sprites.front_default;
+            pokeImage(pokeImg);
+        }
+    });
+}
+
+const pokeImage = (url) => {
+    const pokePhoto = document.getElementById("pokeImg");
+    pokePhoto.src = url;
+}
